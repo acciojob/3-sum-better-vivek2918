05121598483bf628782810862c7053f9b2494b34
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-05-25 22:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function threeSum(arr, target) {
+function threeSum(nums, target) {
 // write your code here
-  
+  nums.sort((a, b) => a - b);
+  let closestSum = Infinity;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+
+      if (sum < target) {
+        left++;
+      } 
+	  else if (sum > target) {
+        right--;
+      } 
+	  else {
+        return closestSum;
+      }
+    }
+  }
+
+  return closestSum;
 }
 
 module.exports = threeSum;
